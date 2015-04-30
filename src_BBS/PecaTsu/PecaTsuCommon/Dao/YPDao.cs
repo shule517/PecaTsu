5ec82f37e75e2e1981dd16fbc4d5305ee14f8dac
shule517 <shule517@gmail.com>
2015-04-30 10:15:46 +0900

    Daoの追加

---
+++ src_BBS/PecaTsu/PecaTsuCommon/Dao/YPDao.cs
@@ -0,0 +1,27 @@
+﻿using PecaTsuCommon.Entity;
+using PecaTsuCommon.Util;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PecaTsuCommon.Dao
+{
+    public class YPDao
+    {
+        public static List<YP> Select()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("SELECT");
+            sb.AppendLine("  `YPUrl`");
+            sb.AppendLine("  , `YPName`");
+            sb.AppendLine("  , `UpdateTime` ");
+            sb.AppendLine("FROM");
+            sb.AppendLine("  `PecaTsuBBS`.`YP` ");
+            sb.AppendLine("WHERE");
+            sb.AppendLine("  `YPUrl` = :YPUrl ");
+            sb.AppendLine("ORDER BY");
+            sb.AppendLine("  `YPUrl`");
+
+            return DBUtil.Select<YP>(sb.ToString());
+        }
+    }
+}
