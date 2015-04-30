5161ec2da75bc9475fac56b149bfd194b3492b5a
shule517 <shule517@gmail.com>
2015-04-30 10:56:12 +0900

    YPDaoにSelectを追加

--- src_BBS/PecaTsu/PecaTsuCommon/Dao/YPDao.cs
+++ src_BBS/PecaTsu/PecaTsuCommon/Dao/YPDao.cs
@@ -5,19 +5,25 @@ using System.Text;
 
 namespace PecaTsuCommon.Dao
 {
+    /// <summary>
+    /// YPDao
+    /// </summary>
     public class YPDao
     {
+        /// <summary>
+        /// YP一覧を取得
+        /// </summary>
+        /// <returns>YP一覧</returns>
         public static List<YP> Select()
         {
             StringBuilder sb = new StringBuilder();
             sb.AppendLine("SELECT");
-            sb.AppendLine("  `YPUrl`");
+            sb.AppendLine("  `YPId`");
+            sb.AppendLine("  , `YPUrl`");
             sb.AppendLine("  , `YPName`");
             sb.AppendLine("  , `UpdateTime` ");
             sb.AppendLine("FROM");
             sb.AppendLine("  `PecaTsuBBS`.`YP` ");
-            sb.AppendLine("WHERE");
-            sb.AppendLine("  `YPUrl` = :YPUrl ");
             sb.AppendLine("ORDER BY");
             sb.AppendLine("  `YPUrl`");
 
--- src_BBS/PecaTsu/PecaTsuCommon/Util/DBUtil.cs
+++ src_BBS/PecaTsu/PecaTsuCommon/Util/DBUtil.cs
@@ -1,14 +1,23 @@
 ﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace PecaTsuCommon.Util
 {
     public class DBUtil
     {
+        /// <summary>
+        /// DBコネクション
+        /// </summary>
         static MySqlConnection connection;
 
+        /// <summary>
+        /// DB接続
+        /// </summary>
         public static void Connect()
         {
             StreamReader sr = new StreamReader(@"Settings/DBAccess.txt", Encoding.GetEncoding("Shift_JIS"));
@@ -17,12 +26,76 @@ namespace PecaTsuCommon.Util
             connection.Open();
         }
 
+        /// <summary>
+        /// DB検索(SELECT)
+        /// </summary>
+        /// <typeparam name="Type">Entityの型</typeparam>
+        /// <param name="sql">SQL文</param>
+        /// <returns>SELECT結果</returns>
         public static List<Type> Select<Type>(string sql)
+            where Type : new()
         {
-            MySqlCommand command = new MySqlCommand(sql, connection);
-            MySqlDataReader reader = command.ExecuteReader();
+            List<Type> entityList = new List<Type>();
 
-            return new List<Type>();
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            using (MySqlDataReader reader = command.ExecuteReader())
+            {
+                // Entityクラス情報を取得
+                PropertyInfo[] properties = typeof(Type).GetProperties();
+
+                // 取得レコードの解析
+                while (reader.Read())
+                {
+                    Type entity = new Type();
+                    GetRecord(reader, properties, entity);
+                    entityList.Add(entity);
+                }
+            }
+
+            return entityList;
+        }
+
+        /// <summary>
+        /// レコードのデータ取得
+        /// </summary>
+        /// <param name="reader">SQLReader</param>
+        /// <param name="properties">Entityのプロパティ情報</param>
+        /// <param name="entity">Entity情報</param>
+        private static void GetRecord(MySqlDataReader reader, PropertyInfo[] properties, object entity)
+        {
+            List<string> data = new List<string>();
+            for (int i = 0; i < reader.FieldCount; i++)
+            {
+                string dbColumnName = reader.GetName(i);
+                PropertyInfo property = properties.Where(elem => elem.Name == dbColumnName).Single();
+
+                if (property.PropertyType == typeof(string))
+                {
+                    string value = reader.GetString(i);
+                    property.SetValue(entity, value);
+                }
+                else if (property.PropertyType == typeof(int))
+                {
+                    int value = reader.GetInt32(i);
+                    property.SetValue(entity, value);
+                }
+                else if (property.PropertyType == typeof(DateTime))
+                {
+                    DateTime value = reader.GetDateTime(i);
+                    property.SetValue(entity, value);
+                }
+                else if (property.PropertyType == typeof(bool))
+                {
+                    bool value = reader.GetBoolean(i);
+                    property.SetValue(entity, value);
+                }
+                else
+                {
+                    throw new Exception("対応していない型(" + reader.GetDataTypeName(i) + ":" + dbColumnName + ")を取得しました。");
+                }
+
+                data.Add(reader.GetString(i));
+            }
         }
     }
 }
